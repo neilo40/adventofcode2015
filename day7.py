ea70ea68a1239eea643075faa9a2a6d229e01c8e
Neil O'Connor <neil@neiloconnor.com>
2015-12-07 22:46:23 +0000

    day7

---
+++ day7.py
@@ -0,0 +1,50 @@
+wires = {}
+values = {}
+with open("inputs/day7.txt", 'r') as fh:
+    lines = fh.readlines()
+for line in lines:
+    parts = line.split()
+    wires[parts[-1]] = parts[0:-2]
+
+
+def is_int(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+def get_value_for_wire(wire):
+    if wire in values:
+        return values[wire]
+    elif is_int(wire):
+        value = int(wire)
+    else:
+        parts = wires[wire]
+        if len(parts) == 1:
+            # 123 -> a
+            # a -> b
+            value = get_value_for_wire(parts[0])
+        elif parts[0] == "NOT":
+            # NOT a -> b
+            value = ~get_value_for_wire(parts[1])
+        else:
+            if parts[1] == "AND":
+                # 1 AND a -> b
+                # a AND b -> c
+                value = get_value_for_wire(parts[0]) & get_value_for_wire(parts[2])
+            elif parts[1] == "OR":
+                # a OR b -> c
+                value = get_value_for_wire(parts[0]) | get_value_for_wire(parts[2])
+            elif parts[1] == "RSHIFT":
+                # a RSHIFT 2 -> c
+                value = get_value_for_wire(parts[0]) >> get_value_for_wire(parts[2])
+            elif parts[1] == "LSHIFT":
+                # a LSHIFT 2 -> c
+                value = get_value_for_wire(parts[0]) << get_value_for_wire(parts[2])
+    values[wire] = value
+    return value
+
+
+print(get_value_for_wire('a'))
