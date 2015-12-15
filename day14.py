ecef52ea9e5d22a8e0d99dc7c77f9f6312ad044a
Neil O'Connor <neil@neiloconnor.com>
2015-12-15 20:34:07 +0000

    day14

--- day14.py
+++ day14.py
@@ -11,10 +11,35 @@ for line in lines:
     rest = int(parts[13])
     schedule = []
     while len(schedule) < 2503:
-        schedule += [speed,] * duration
-        schedule += [0,] * rest
+        schedule += [speed, ] * duration
+        schedule += [0, ] * rest
     schedules[name] = schedule
 
 
 for reindeer in schedules:
     print("{} flew {} in 2503 seconds".format(reindeer, sum(schedules[reindeer][:2503])))
+
+
+scores = {}
+for reindeer in schedules:
+    scores.update({reindeer: {"dist": 0, "score": 0}})
+
+
+for sec in range(2503):
+    for reindeer in scores:
+        scores[reindeer]["dist"] += schedules[reindeer][sec]
+    furthest = 0
+    winning = []
+    for reindeer in scores:
+        if scores[reindeer]["dist"] == furthest:
+            winning.append(reindeer)
+        elif scores[reindeer]["dist"] > furthest:
+            furthest = scores[reindeer]["dist"]
+            winning = [reindeer, ]
+    for reindeer in winning:
+        scores[reindeer]["score"] += 1
+
+
+from pprint import pprint as pp
+pp(scores)
+
